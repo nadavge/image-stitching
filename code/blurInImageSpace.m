9e65b78518ecf7b314a456687ea334c2ae1242db
Nadav Geva <nadavgeva4@gmail.com>
2015-12-30 23:04:22 +0200

    Update blur function

--- code/blurInImageSpace.m
+++ code/blurInImageSpace.m
@@ -3,7 +3,7 @@ function [ blurred_im ] = blurInImageSpace( im, kernelSize )
 %   Blur an image in image space using gaussian kernel convolution
     
     %% Blur image
-    kernel = makeKernel(kernelSize);
-    blurred_im = conv2(im, kernel, 'same');
+    filter = makeFilter(kernelSize);
+    blurred_im = applyFilter( im, filter );
 
 end
